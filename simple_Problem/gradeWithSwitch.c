6d8f456782541c38c2fb32c5351ef0b8351f18cf
Shakib <shakibc83@gmail.com.com>
2022-09-17 03:39:34 +0600

    first commit

---
+++ simple_Problem/gradeWithSwitch.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+    int score,grade;
+    printf("Enter your score: ");
+    scanf("%d",&score);
+
+
+    grade = score / 10;
+
+    switch(grade){
+    case 10:
+    case  9:
+        printf(" \n\a your grade is A ");
+        break;
+         case  8:
+        printf(" \n\a your grade is B ");
+        break;
+         case  7:
+        printf(" \n\a your grade is C ");
+        break;
+         case  6:
+        printf(" \n\a your grade is D ");
+        break;
+         default:
+        printf(" \n\a your grade is F ");
+        break;
+
+    }
+    getch();
+}
