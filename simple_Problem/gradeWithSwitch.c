b1b83d05537080c41ad0d312750e82af7086132e
Shakib <shakibc83@gmail.com.com>
2022-09-17 03:52:49 +0600

    Add_semicolon

--- simple_Problem/gradeWithSwitch.c
+++ simple_Problem/gradeWithSwitch.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int main()
+void main()
 {
     int score,grade;
     printf("Enter your score: ");
@@ -11,21 +11,21 @@ int main()
     switch(grade){
     case 10:
     case  9:
-        printf(" \n\a your grade is A ");
+        printf(" \n\a your grade is: A ");
         break;
          case  8:
-        printf(" \n\a your grade is B ");
+        printf(" \n\a your grade is: B ");
         break;
          case  7:
-        printf(" \n\a your grade is C ");
+        printf(" \n\a your grade is: C ");
         break;
          case  6:
-        printf(" \n\a your grade is D ");
+        printf(" \n\a your grade is: D ");
         break;
          default:
-        printf(" \n\a your grade is F ");
+        printf(" \n\a your grade is:F ");
         break;
 
     }
-    
+    getch();
 }
