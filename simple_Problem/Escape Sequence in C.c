5374c7679be189061f06f1da12c4a763c4569093
Shakib <shakibc83@gmail.com.com>
2022-09-22 13:50:20 +0600

    ASCIIValueToChracter

---
+++ simple_Problem/Escape Sequence in C.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    printf("Hello\n World");
+    printf("Hello\t World");
+    printf("Hello\' World \'");
+    printf("Hello\" World \"");
+return 0;
+}
---
+++ simple_Problem/assciidemo1.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+   int n;
+   printf("Enter any ASCII value: ");
+   scanf("%d",&n);
+
+   printf("The Ascii chracter is:%c",n);
+return 0;
+}
