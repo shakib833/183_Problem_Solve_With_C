f1dc07d985882171fe17f51888b89643c915d688
Shakib <shakibc83@gmail.com.com>
2022-09-23 00:56:52 +0600

    AddTwoNumber

---
+++ simple_Problem/AddTwoNumber.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int num1,num2,sum; 
+    printf("Enter Two Number: ");
+    scanf("%d %d",&num1,&num2);
+
+    sum = num1+num2;
+    printf("The sum is: %d ",sum); // sum na likhe amra direct num1+num2 Ooo likte partam
+    return 0;
+}
