e4cd4ea8b6ccf6fc446dd0cc5b70d47b7ce110ec
Shakib <shakibc83@gmail.com.com>
2022-09-24 02:45:45 +0600

    find the middle Niumber

---
+++ simple_Problem/BoundaryOfRectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    int L,W,Boundary;
+    scanf("%d %d",&L,&W);
+    Boundary = 2*(L+W);   
+    printf("%d \n",Boundary);
+   
+    return 0;
+}
---
+++ simple_Problem/findTheMiddleNumber.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    int L,W,MiddleNumber;
+    scanf("%d %d",&L,&W);
+    MiddleNumber = (L+W)/2;   
+    printf("Number = %d \n",MiddleNumber);
+   
+    return 0;
+}
